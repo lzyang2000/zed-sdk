f780a259a749c9f35c8c524a09e38206faeb0036
Lizhi Yang <lzyang@caltech.edu>
2023-07-18 05:45:46 +0000

    remove all viewers from detector.py

--- object detection/custom detector/python/pytorch_yolov8/detector.py
+++ object detection/custom detector/python/pytorch_yolov8/detector.py
@@ -132,27 +132,28 @@ def main():
     camera_infos = zed.get_camera_information()
     camera_res = camera_infos.camera_configuration.resolution
     # Create OpenGL viewer
-    viewer = gl.GLViewer()
+    # viewer = gl.GLViewer()
     point_cloud_res = sl.Resolution(min(camera_res.width, 720), min(camera_res.height, 404))
-    point_cloud_render = sl.Mat()
-    viewer.init(camera_infos.camera_model, point_cloud_res, obj_param.enable_tracking)
+    # point_cloud_render = sl.Mat()
+    # viewer.init(camera_infos.camera_model, point_cloud_res, obj_param.enable_tracking)
     point_cloud = sl.Mat(point_cloud_res.width, point_cloud_res.height, sl.MAT_TYPE.F32_C4, sl.MEM.CPU)
     image_left = sl.Mat()
-    # Utilities for 2D display
+    # # Utilities for 2D display
     display_resolution = sl.Resolution(min(camera_res.width, 1280), min(camera_res.height, 720))
-    image_scale = [display_resolution.width / camera_res.width, display_resolution.height / camera_res.height]
+    # image_scale = [display_resolution.width / camera_res.width, display_resolution.height / camera_res.height]
     image_left_ocv = np.full((display_resolution.height, display_resolution.width, 4), [245, 239, 239, 255], np.uint8)
 
     # Utilities for tracks view
-    camera_config = camera_infos.camera_configuration
-    tracks_resolution = sl.Resolution(400, display_resolution.height)
-    track_view_generator = cv_viewer.TrackingViewer(tracks_resolution, camera_config.fps, init_params.depth_maximum_distance)
-    track_view_generator.set_camera_calibration(camera_config.calibration_parameters)
-    image_track_ocv = np.zeros((tracks_resolution.height, tracks_resolution.width, 4), np.uint8)
+    # camera_config = camera_infos.camera_configuration
+    # tracks_resolution = sl.Resolution(400, display_resolution.height)
+    # track_view_generator = cv_viewer.TrackingViewer(tracks_resolution, camera_config.fps, init_params.depth_maximum_distance)
+    # track_view_generator.set_camera_calibration(camera_config.calibration_parameters)
+    # image_track_ocv = np.zeros((tracks_resolution.height, tracks_resolution.width, 4), np.uint8)
     # Camera pose
     cam_w_pose = sl.Pose()
 
-    while viewer.is_available() and not exit_signal:
+    # while viewer.is_available() and not exit_signal:
+    while not exit_signal:
         if zed.grab(runtime_params) == sl.ERROR_CODE.SUCCESS:
             # -- Get the image
             lock.acquire()
@@ -172,30 +173,30 @@ def main():
             lock.release()
             zed.retrieve_objects(objects, obj_runtime_param)
 
-            # -- Display
-            # Retrieve display data
+            # # -- Display
+            # # Retrieve display data
             zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA, sl.MEM.CPU, point_cloud_res)
-            point_cloud.copy_to(point_cloud_render)
+            # point_cloud.copy_to(point_cloud_render)
             zed.retrieve_image(image_left, sl.VIEW.LEFT, sl.MEM.CPU, display_resolution)
             zed.get_position(cam_w_pose, sl.REFERENCE_FRAME.WORLD)
 
             # 3D rendering
-            viewer.updateData(point_cloud_render, objects)
-            # 2D rendering
+            # viewer.updateData(point_cloud_render, objects)
+            # # 2D rendering
             np.copyto(image_left_ocv, image_left.get_data())
-            cv_viewer.render_2D(image_left_ocv, image_scale, objects, obj_param.enable_tracking)
-            global_image = cv2.hconcat([image_left_ocv, image_track_ocv])
-            # Tracking view
-            track_view_generator.generate_view(objects, cam_w_pose, image_track_ocv, objects.is_tracked)
+            # cv_viewer.render_2D(image_left_ocv, image_scale, objects, obj_param.enable_tracking)
+            # global_image = cv2.hconcat([image_left_ocv, image_track_ocv])
+            # # Tracking view
+            # track_view_generator.generate_view(objects, cam_w_pose, image_track_ocv, objects.is_tracked)
 
-            cv2.imshow("ZED | 2D View and Birds View", global_image)
-            key = cv2.waitKey(10)
-            if key == 27:
-                exit_signal = True
+            # cv2.imshow("ZED | 2D View and Birds View", global_image)
+            # key = cv2.waitKey(10)
+            # if key == 27:
+            #     exit_signal = True
         else:
             exit_signal = True
 
-    viewer.exit()
+    # viewer.exit()
     exit_signal = True
     zed.close()
 
